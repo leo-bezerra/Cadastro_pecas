d87822c36fe66d8687d9d511aa6a079b8a6c80b4
leo-bezerra <tubzleo@gmail.com>
2021-12-23 15:52:29 -0300

    Projeto cadastro de peças ok

--- pecas.js
+++ pecas.js
@@ -6,26 +6,25 @@
 let pesoPeca = 100
 
 if(pesoPeca > 100) {
-    console.log("Peso maior que 100g, logo podemos cadastrar a peça!")
+    console.log("Peso maior que 100g, a peça pode ser cadastrada!")
 } else {
-    console.log("Peso insuficiente, não é possível cadastrar.")
+    console.log("Peso insuficiente, não é possível cadastrar a peça!")
 }
 
 let numeroPecas = 10
 
 if(numeroPecas < 10) {
-console.log("Ainda há espaço na caixa, podemos cadastrar a peça.")
+console.log("Ainda há espaço na caixa, a peça pode ser cadastrada.")
 } else {
     console.log("Não há espaço disponível na caixa.")
 }
-
 
 let nomePeca = "Disco"
 
 console.log("O comprimento no nome da peça é:", nomePeca.length)
 
 if(nomePeca.length < 3){
-    console.log("Nome de peça muito curto, não é possível cadastrar")
+    console.log("Nome de peça muito curto, não é possível efetuar o cadastro")
 } else {
-    console.log("Nome de peça adequado, podemos cadastrar")
+    console.log("Nome de peça em conformidade, pode ser cadastrada!")
 }
