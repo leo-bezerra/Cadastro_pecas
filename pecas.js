42be5548586069af38375b59e0ae8a45b8206447
leo-bezerra <tubzleo@gmail.com>
2021-12-22 22:18:11 -0300

    Envio do pecas.js

---
+++ pecas.js
@@ -0,0 +1,13 @@
+// Variáveis, são um espaço na memória do computador para guardar algum valor.
+// pesoPeca = Camel Case
+// peso_peca = Snake Case
+// PesoPeca = Pascal Case
+
+let pesoPeca = 100
+
+if(pesoPeca > 100) {
+    console.log("Peso maior que 100g, logo podemos cadastrar a peça!")
+} else {
+    console.log("Peso insuficiente, não é possível cadastrar.")
+}
+
