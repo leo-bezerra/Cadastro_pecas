03e6da65dfcbba1294000d4a4643f907a90acef8
leo-bezerra <tubzleo@gmail.com>
2021-12-23 11:12:01 -0300

    Arquivo do sistema de peças ok

--- pecas.js
+++ pecas.js
@@ -11,3 +11,21 @@ if(pesoPeca > 100) {
     console.log("Peso insuficiente, não é possível cadastrar.")
 }
 
+let numeroPecas = 10
+
+if(numeroPecas < 10) {
+console.log("Ainda há espaço na caixa, podemos cadastrar a peça.")
+} else {
+    console.log("Não há espaço disponível na caixa.")
+}
+
+
+let nomePeca = "Disco"
+
+console.log("O comprimento no nome da peça é:", nomePeca.length)
+
+if(nomePeca.length < 3){
+    console.log("Nome de peça muito curto, não é possível cadastrar")
+} else {
+    console.log("Nome de peça adequado, podemos cadastrar")
+}
